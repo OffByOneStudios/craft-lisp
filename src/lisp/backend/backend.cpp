1cfdfc20e341da536c88ba935ba477a5b9a9e069
Mason Bially <mason.bially@gmail.com>
2018-04-26 14:14:43 -0700

    Removing outdated backend code.

--- src/lisp/backend/backend.cpp
+++ src/lisp/backend/backend.cpp
@@ -7,13 +7,3 @@ using namespace craft;
 using namespace craft::types;
 using namespace craft::lisp;
 
-instance<PBackend> craft::lisp::llvm_backend_provider()
-{
-	return instance<>();
-	//return instance<PBackend>::forType<LlvmBackend>();
-}
-
-instance<PBackend> craft::lisp::bootstrap_backend_provider()
-{
-	return instance<PBackend>::forType<BootstrapInterpreter>();
-}
--- src/lisp/backend/backend.h
+++ src/lisp/backend/backend.h
@@ -3,9 +3,3 @@
 #include "lisp/common.h"
 #include "lisp/lisp.h"
 
-namespace craft {
-namespace lisp
-{
-	instance<PBackend> llvm_backend_provider();
-	instance<PBackend> bootstrap_backend_provider();
-}}
